5986392a189273f6e2e12a2ffcdcbb02a1f39e9b
SaberLS <patryk.platek19@gmail.com>
2023-11-09 21:32:51 +0100

    small design changes in postCard

--- src/features/posts/PostCard.js
+++ src/features/posts/PostCard.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { Card, CardBody, CardHeader, Box } from "grommet";
+import { Card, CardBody, CardHeader, Box, Heading, Grid } from "grommet";
+import { Ascending, Descending } from "grommet-icons";
 
 export default function PostCard({ title, content }) {
     return (
         <Box align="center">
-            <Card name="Post" direction="column" justify="start" gap="none" margin={{"top": "medium"}} width="large">
-                <CardHeader direction="row" justify="start" pad="small" align="start"  overflow="hidden">{ title }</CardHeader>
-                <CardBody pad="large">{ content }</CardBody>
+            <Card name="Post" direction="column" margin={{"top": "medium"}} height="large" width="large">
+                <CardHeader direction="row" justify="between" pad="small" align="center">
+                    <Grid columns={["full"]} rows={["1/2", "1/2"]} height="xsmall" width="xxsmall">
+                        <Box justify="center" align="center">
+                            <Ascending size="large" />
+                        </Box>
+                        <Box justify="center" align="center">
+                            <Descending size="large" />
+                        </Box>
+                    </Grid>
+                    <Box justify="start" align="start" fill="horizontal">
+                        <Heading level="2">{title}</Heading>
+                    </Box>
+                </CardHeader>
+                <CardBody pad="small">{ content }</CardBody>
             </Card>
         </Box>
 
--- src/features/posts/Posts.js
+++ src/features/posts/Posts.js
@@ -5,14 +5,20 @@ import { requestPosts,
          selectLastName } from "./postsSlice";
 import ShakaPlayer from 'shaka-player-react';
 import 'shaka-player-react/dist/controls.css';
-import { Box, Button as GrommetButton, Card } from 'grommet';
+import { Box, Button as GrommetButton, Card, Image, Anchor } from 'grommet';
 import PostCard from "./PostCard";
-import { Image } from "grommet";
+
 
 export default function User() {
     const dispatch = useDispatch();
     const lastPostName = useSelector(selectLastName);
     const posts = useSelector(selectPosts);
+
+
+    /* <Card name="Post" direction="column" justify="start" pad="xsmall" gap="none" margin={{"top": "medium"}} width="100%" key={post}>
+                    <h3>{posts[post].title}</h3>\
+                    <a target="blank" href={posts[post].url}>{posts[post].url}</a>
+                </Card>*/
 
     return (
         <>
@@ -23,20 +29,42 @@ export default function User() {
             <Box direction="column">{Object.keys(posts).map((post) => {
                 if(posts[post].type === 'image'){
                     return (
-                        <PostCard key={post} id={post} title={posts[post].title} content={<Image fit="cover" src={posts[post].url} />} />
+                        <PostCard 
+                            key={post}
+                            id={post}
+                            title={posts[post].title}
+                            content={<Image fit="contain" fill="vertical" src={posts[post].url} />} 
+                        />
                     )
                 }else if(posts[post].type === 'hosted:video') {
-                    return <Card name="Post" direction="column" justify="start" pad="xsmall" gap="none" margin={{"top": "medium"}} width="100%" key={post}>
-                    <h3>{posts[post].title}</h3>\
-                    <div style={{ margin: 'auto', height: 700, width: 400 }}>
-                        <ShakaPlayer height={700} muted={true} loop={true} autoPlay src={posts[post].video} />
-                    </div>
-                </Card>
+                    return (
+                        <PostCard
+                            key={post}
+                            id={post}
+                            title={posts[post].title}
+                            content={
+                                <Box direction="row" justify="center" align="center" fit="contain" fill="horizontal">
+                                    <div style={{width: 350}}>
+                                        <ShakaPlayer muted={true} loop={true} autoPlay src={posts[post].video} />
+                                    </div>
+                                </Box>
+                                }
+                        />
+                    )
                 }else if(posts[post].type === 'link'){
-                    return <Card name="Post" direction="column" justify="start" pad="xsmall" gap="none" margin={{"top": "medium"}} width="100%" key={post}>
-                    <h3>{posts[post].title}</h3>\
-                    <a target="blank" href={posts[post].url}>{posts[post].url}</a>
-                </Card>
+                    return (
+                        <PostCard
+                            key={post}
+                            id={post}
+                            title={posts[post.title]}
+                            content={
+                                <Box direction="row" align="start" justify="center" fit="contain" fill="horizontal">
+                                    <Anchor label={posts[post].url} href={posts[post].url} />
+                                    <Image fit="contain" src={posts[post].thumbnail} />
+                                </Box>
+                            }
+                        />
+                    )
                 }
                 return <Card name="Post" direction="column" justify="start" pad="xsmall" gap="none" margin={{"top": "medium"}} width="100%" key={post}>{posts[post].title}
                     {console.log("UNRECOGNIZED!!! :", posts[post])}
